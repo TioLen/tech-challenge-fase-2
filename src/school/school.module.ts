80b1f4df3bb52a6ec9a19d063765a75ce67dc1f4
Gabriel Sanz Sztochryn <gabrielsanz2106@gmail.com>
2025-10-12 19:50:30 -0300

    school modules atualizado

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,4 +1,30 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Docente, DocenteSchema } from './schemas/docente.schema';
+import { Aluno, AlunoSchema } from './schemas/aluno.schema';
+import { Turma, TurmaSchema } from './schemas/turma.schema';
+import { Post, PostSchema } from './schemas/post.schema';
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: Docente.name,
+                schema: DocenteSchema
+            },
+            {
+                name: Aluno.name,
+                schema: AlunoSchema
+            },
+            {
+                name: Turma.name,
+                schema: TurmaSchema
+            },
+            {
+                name: Post.name,
+                schema: PostSchema
+            },
+        ]),
+    ],
+})
 export class SchoolModule {}
